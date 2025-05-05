da49d422bc289db52968c8d46e031e9c2952519b
ungchaeNa <ucnadev3@gmail.com>
2025-05-05 22:07:13 +0900

    Create & Drop DB doitsql

---
+++ queries/query03.sql
@@ -0,0 +1,7 @@
+-- 쿼리 실행 단축키? Ctrl + Enter
+-- 데이터베이스에선 항상 대소문자를 잘 구분해서 참조할 것.
+CREATE DATABASE DoItSQL;
+DROP DATABASE DoItSQL;
+-- 실무에서는 여러 프로그램들이 데이터베이스에 접속해 사용하는 경우가 많아서
+-- 데이터베이스를 삭제하는 일은 드물다.
+
