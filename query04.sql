c48c85956afbabe8348b084cc6f205cecb3bcb31
ungchaeNa <ucnadev3@gmail.com>
2025-05-07 23:29:44 +0900

    SHOW COLUMNS FROM table

--- query04.sql
+++ query04.sql
@@ -8,5 +8,10 @@ SELECT first_name FROM customer;
 
 -- 2개의 열 조회
 SELECT first_name, last_name FROM customer;
--- 전체 열 조회
+-- 전체 열 조회(※ 전체 열 조회는 자원을 많이 소비)
 SELECT * FROM customer;
+
+-- SHOW COLUMNS FROM [테이블명];
+-- :테이블의 구조(schema)를 확인할 때 사용하는 쿼리
+-- customer 테이블의 열 정보 조회
+SHOW COLUMNS FROM sakila.customer;
