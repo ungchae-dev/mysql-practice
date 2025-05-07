aeab44b2d0d31fb18b3461d14203b54eb040b219
ungchaeNa <ucnadev3@gmail.com>
2025-05-07 23:51:44 +0900

    Apply comparison operators in WHERE clause

--- query04.sql
+++ query04.sql
@@ -19,3 +19,32 @@ SHOW COLUMNS FROM sakila.customer;
 -- WHERE 절, = 연산자로 특정 값 조회
 SELECT * FROM customer
 WHERE first_name = 'MARIA';
+
+-- address_id가 200인 행 조회
+SELECT * FROM customer 
+WHERE address_id = 200;
+
+-- address_id가 200 미만인 행 조회
+SELECT * FROM customer 
+WHERE address_id < 200;
+
+-- first_name이 MARIA인 행 조회
+SELECT * FROM customer 
+WHERE first_name = 'MARIA';
+
+-- first_name이 MARIA 미만인 행 조회
+-- (first_name 열에서 데이터가 A, B, C 순으로 
+-- MARIA 보다 앞에 위치한 행들)
+SELECT * FROM customer 
+WHERE first_name < 'MARIA';
+
+-- payment 테이블에서 
+-- payment_date가 2005-07-09 13:24:07인 행 조회
+SELECT * FROM payment 
+WHERE payment_date = '2005-07-09 13:24:07';
+
+-- payment 테이블에서 
+-- payment_date가 2005년 7월 9일 미만인 행 조회
+SELECT * FROM payment 
+WHERE payment_date < '2005-07-09';
+-- WHERE 절에서 비교 연산자 사용 Fin.
