d77f5fadb1d7429b6ba8a5116e4e03f92999e83a
ungchaeNa <ucnadev3@gmail.com>
2025-05-07 23:21:33 +0900

    Select columns from customer

--- query04.sql
+++ query04.sql
@@ -5,3 +5,8 @@ USE sakila;
 -- sakila 데이터베이스의 customer 테이블 조회하기
 -- first_name 열을 조회
 SELECT first_name FROM customer;
+
+-- 2개의 열 조회
+SELECT first_name, last_name FROM customer;
+-- 전체 열 조회
+SELECT * FROM customer;
